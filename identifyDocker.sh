a8647045b2edd49706fd6f58ce9c7c3f6457efb6
K1LLUM1N471 <K1LLUM1N471@users.noreply.github.com>
2021-12-15 11:10:00 +0100

    Create identifyDocker.sh

---
+++ identifyDocker.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+# 2021-12-14 14:12
+
+################
+# CONFIGURATON #
+################
+
+HITS="hits.txt"
+HITS_CLEANED="hits_cleaned.txt"
+DOCKER_NAMES="docker-names.txt"
+
+#############
+# FUNCTIONS #
+#############
+
+########
+# MAIN #
+########
+
+clear
+
+# https://github.com/mergebase/log4j-detector
+if [ ! -f $HITS ]
+then
+   ./jdk-11/bin/java -jar log4j-detector-2021.12.13.jar /var > $HITS
+fi
+
+# Skip first lines and all problems
+cat $HITS | grep -v "Problem" | grep -v "\-\-" > $HITS_CLEANED
+
+docker ps -a --format {{.Names}} > $DOCKER_NAMES
+
+while read FOUND_LINE
+do
+#   echo "FOUND_LINE: '$FOUND_LINE'"
+
+   LAYER_ID=$(echo "$FOUND_LINE" | cut -d"/" -f6)
+#   echo "LAYER_ID: '$LAYER_ID'"
+
+   while read DOCKER_NAME
+   do
+#      echo "DOCKER_NAME: '$DOCKER_NAME'"
+
+      if [ ! -z "$(docker inspect $DOCKER_NAME | grep $LAYER_ID)" ]
+      then
+         echo "$FOUND_LINE"
+         echo "Found in '$DOCKER_NAME'"
+         echo ""
+      fi
+   done < $DOCKER_NAMES
+done < $HITS_CLEANED
